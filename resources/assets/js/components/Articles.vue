43897e77b9a3c39e5d9d7dd6722fdf3cc5c6dc51
Max Bernard <max@tower-1.homebase.net>
2018-10-21 19:30:16 -0400

    First working Vue.js version

---
+++ resources/assets/js/components/Articles.vue
@@ -0,0 +1,186 @@
+<template>
+  <div id="app" class="container-fluid">
+    <div class="mt-2">
+      <form @submit.prevent="saveArticle" class="mb-2">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Title" v-model="article.title">
+        </div>
+        <div class="form-group">
+          <textarea class="form-control" rows="4" cols="100" placeholder="Body" v-model="article.body"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary btn-xs">Save</button>
+      </form>
+    </div>
+    <h3>Articles</h3>
+    <div>
+      <table id="articleTable" class="table table-hover table-bordered" width="100%">
+        <thead  class="blue-grey lighten-4">
+          <tr class="text-center">
+            <th>ID</th>
+            <th>Title</th>
+            <th class="text-center" width="190px">
+            <button @click="newArticle()" class="add-modal btn btn-success btn-xs">
+                <i class="fa fa-plus ml-1"></i> New Article
+            </button>
+            </th>
+          </tr>
+        </thead>
+        <tbody v-for="article in articles" :key="article.id">
+          <tr style="padding: 2px; font-size:1em; margin-bottom:1px;">
+            <td>{{ article.id }}</td>
+            <td>{{ article.title }}</td>
+            <td>
+              <button @click="editArticle(article)" class="btn btn-info btn-xs"><i class="fa fa-eye ml-1"></i> Show</button>
+              <button @click="editArticle(article)" class="btn btn-warning btn-xs"><i class="fa fa-pencil ml-1"></i> Edit</button>
+              <button @click="deleteArticle(article.id)" class="btn btn-danger btn-xs"><i class="fa fa-trash ml-1"></i> Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <div style='clear:both'>
+        <span class="pull-left">
+        <p>Showing {{pagination.from}} to {{pagination.to}} of {{pagination.total}} articles</p>
+        </span>
+        <span class="pull-right">
+          <nav aria-label="Page navigation">
+            <ul class="pagination navbar-right">
+            <li class="page-item" :class="[{disabled: !pagination.prev_page_url}]">
+                <a href="#" class="page-link first" @click="fetchArticles(pagination.first_page_url)"><i class="fa fa-angle-double-left"></i></a>
+            </li>
+            <li class="page-item" :class="[{disabled: !pagination.prev_page_url}]">
+                <a href="#" class="page-link previous round" @click="fetchArticles(pagination.prev_page_url)"><i class="fa fa-angle-left"></i></a>
+            </li>
+            <li class="page-item disabled"><a href="#" class="page-link text-dark">Page {{ pagination.current_page }} of {{ pagination.last_page }}</a></li>
+            <li class="page-item" :class="[{disabled: !pagination.next_page_url}]">
+                <a href="#" class="page-link next" @click="fetchArticles(pagination.next_page_url)"><i class="fa fa-angle-right"></i></a>
+            </li>
+            <li class="page-item" :class="[{disabled: !pagination.next_page_url}]">
+                <a href="#" class="page-link last" @click="fetchArticles(pagination.last_page_url)"><i class="fa fa-angle-double-right"></i></a>
+            </li>
+            </ul>
+          </nav>
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  //$(document).ready( function () {
+  //    var table = $('#articleTable').DataTable({
+  //    });
+  //});
+
+  export default {
+    data() {
+      return {
+        articles: [],
+        article: {
+          id: '',
+          title: '',
+          body: ''
+        },
+        article_id: '',
+        pagination: {},
+        edit: false
+      }
+    },
+    created() {
+      this.fetchArticles();
+    },
+    methods: {
+      fetchArticles(page_url) {
+        let vm = this;
+        page_url = page_url || '/api/articles';
+        fetch(page_url)
+          .then(res => res.json())
+          .then(res => {
+            this.articles = res.data;
+            vm.makePagination(res.meta, res.links);
+          })
+        .catch(err => console.log(err));
+      },
+      makePagination(meta, links) {
+        let pagination = {
+          current_page: meta.current_page,
+          first_page: meta.first_page,
+          last_page: meta.last_page,
+          from: meta.from,
+          to: meta.to,
+          total: meta.total,
+          next_page_url: links.next,
+          prev_page_url: links.prev,
+          last_page_url: links.last
+        }
+        this.pagination = pagination;
+      },
+      deleteArticle(id) {
+        if(confirm('Are you sure?')) {
+          fetch(`api/article/${id}`, {
+            method: 'delete'
+          })
+          .then(res => res.json())
+          .then(data => {
+            alert('Article removed.');
+            this.fetchArticles();
+          })
+          .catch(err => console.log(err));
+        }
+      },
+      saveArticle() {
+        if(this.edit === false) {
+          // Add
+          fetch('api/article', {
+            method: 'POST',
+            body: JSON.stringify(this.article),
+            headers: {
+                'content-type': 'application/json'
+            }
+          })
+          .then(res => res.json())
+          .then(data => {
+            this.article.title = '';
+            this.article.body = '';
+            alert('Article added');
+            this.fetchArticles();
+          })
+        } else {
+          // Update
+          fetch('api/article', {
+            method: 'PUT',
+            body: JSON.stringify(this.article),
+            headers: {
+                'content-type': 'application/json'
+            }
+          })
+          .then(res => res.json())
+          .then(data => {
+            this.article.title = '';
+            this.article.body = '';
+            alert('Article updated');
+            this.fetchArticles();
+          })
+        }
+      },
+      editArticle(article) {
+        this.edit = true;
+        this.article.id = article.id;
+        this.article.article_id = article.id;
+        this.article.title = article.title;
+        this.article.body = article.body;
+      },
+      newArticle(article) {
+        this.edit = false;
+        this.article.id = '';
+        this.article.article_id = '';
+        this.article.title = '';
+        this.article.body = '';
+      }
+    },
+    created () {
+      this.fetchArticles();
+    }
+  };
+</script>
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,39 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script>window.Laravel = { csrfToken: '{{ csrf_token() }}' }</script>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <title>L5Articles</title>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- Styles -->
+    <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <!--link href="css/jquery.dataTables.min.css" rel="stylesheet"-->
+    <!--link href="css/datatables.bootstrap.min.css" rel="stylesheet"-->
+    <link href="css/style.css" rel="stylesheet">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+    <!-- Scripts -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!--script src="js/datatables.min.js"></script-->
+    <!--script src="js/jquery.datatables.min.js"></script-->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+  </head>
+  <body>
+    <div id="app">
+      <navbar></navbar>
+      <div class="container">
+        <articles></articles>
+      </div>
+    </div>
+  </body>
 </html>
